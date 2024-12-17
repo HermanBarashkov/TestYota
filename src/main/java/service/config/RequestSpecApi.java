9c32e672feb76f5b6dff1952e603bc49e04ad780
anton <anton@example.com>
2024-12-17 15:20:08 +0300

    All fix

--- src/main/java/service/config/RequestSpecApi.java
+++ src/main/java/service/config/RequestSpecApi.java
@@ -3,21 +3,19 @@ package service.config;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
-import io.restassured.specification.ResponseSpecification;
-import io.restassured.builder.ResponseSpecBuilder;
 
 
 public class RequestSpecApi {
 
     public static final RequestSpecification REQUEST_SPECIFICATION_JSON =
             new RequestSpecBuilder()
-                    .setBaseUri("http://127.0.0.1:8090")
+                    .setBaseUri("http://10.2.1.4:8080")
                     .setContentType(ContentType.JSON)
                     .build();
 
     public static final RequestSpecification REQUEST_SPECIFICATION_XML =
             new RequestSpecBuilder()
-                    .setBaseUri("http://127.0.0.1:8090")
+                    .setBaseUri("http://10.2.1.4:8080")
                     .setContentType(ContentType.XML)
                     .build();
 }
