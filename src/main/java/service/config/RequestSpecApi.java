21ee426c5942da987ad0ae29b858fc89242cc4fd
anton <anton@example.com>
2024-12-17 14:58:38 +0300

    mb fix

--- src/main/java/service/config/RequestSpecApi.java
+++ src/main/java/service/config/RequestSpecApi.java
@@ -11,13 +11,13 @@ public class RequestSpecApi {
 
     public static final RequestSpecification REQUEST_SPECIFICATION_JSON =
             new RequestSpecBuilder()
-                    .setBaseUri("http://localhost:8099")
+                    .setBaseUri("http://localhost:8090")
                     .setContentType(ContentType.JSON)
                     .build();
 
     public static final RequestSpecification REQUEST_SPECIFICATION_XML =
             new RequestSpecBuilder()
-                    .setBaseUri("http://localhost:8099")
+                    .setBaseUri("http://localhost:8090")
                     .setContentType(ContentType.XML)
                     .build();
 }
