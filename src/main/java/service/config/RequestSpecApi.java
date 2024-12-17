67539e418a1ddc06e5e38b12625f4f1dfbea7b44
anton <anton@example.com>
2024-12-17 12:57:35 +0300

    mb fix

--- src/main/java/service/config/RequestSpecApi.java
+++ src/main/java/service/config/RequestSpecApi.java
@@ -3,21 +3,18 @@ package service.config;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
-import io.restassured.specification.ResponseSpecification;
-import io.restassured.builder.ResponseSpecBuilder;
-
 
 public class RequestSpecApi {
 
     public static final RequestSpecification REQUEST_SPECIFICATION_JSON =
             new RequestSpecBuilder()
-                    .setBaseUri("http://localhost:8099")
+                    .setBaseUri("http://localhost:8080")
                     .setContentType(ContentType.JSON)
                     .build();
 
     public static final RequestSpecification REQUEST_SPECIFICATION_XML =
             new RequestSpecBuilder()
-                    .setBaseUri("http://localhost:8099")
+                    .setBaseUri("http://localhost:8080")
                     .setContentType(ContentType.XML)
                     .build();
 }
